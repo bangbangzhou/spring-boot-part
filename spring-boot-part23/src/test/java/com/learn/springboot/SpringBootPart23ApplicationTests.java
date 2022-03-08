bebdde1f462626812b2c7137b490e63b9b3e1b49
Administrator <825505212@qq.com>
2022-03-08 23:22:36 +0800

    SpringBoot-23-全局异常机制+RESTful统一规范

---
+++ spring-boot-part23/src/test/java/com/learn/springboot/SpringBootPart23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPart23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

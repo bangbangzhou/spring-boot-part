1e9980041d7771c70360d9e8ce67121c7b728e0d
Administrator <825505212@qq.com>
2022-03-09 19:36:30 +0800

    SpringBoot-23-全局异常机制+RESTful统一规范

---
+++ spring-boot-part24/src/main/java/com/learn/springboot/SpringBootPart24Application.java
@@ -0,0 +1,13 @@
+package com.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPart24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPart24Application.class, args);
+    }
+
+}
